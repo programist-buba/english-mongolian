b194df9fa0893d59b2a638b81f1a8b265c8ca448
Tushig Biliguun <Tushig@boldoo.local>
2019-09-14 13:10:09 +0800

    adfds

--- style.js
+++ style.js
@@ -1,15 +1,11 @@
 
 function green() {
     document.getElementById("word1").style.backgroundColor = "green";
+    document.getElementById("word1").classList.add("check");
 }
 
 function red() {
     document.getElementById("word2").style.backgroundColor = "red";
     document.getElementById("word1").style.backgroundColor = "green";
-    document.getElementById("word2").classList.add("shakemepls");
-    document.getElementById("word2").classList.remove("shakemepls");
-}
-
-function shakeme() {
     document.getElementById("word2").classList.add("shake");
 }
