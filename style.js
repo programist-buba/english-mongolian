53233a4f0811cab014b401143da4385d9fe8f66f
Tushig Biliguun <Tushig@boldoo.local>
2019-09-16 18:00:59 +0800

    adfds

--- style.js
+++ style.js
@@ -1,9 +1,15 @@
+let val = 0;
+correct = 10; 
+wrong = 5;
 
 function green() {
     document.getElementById("word1").style.backgroundColor = "green";
     document.getElementById("word1").classList.add("shakeme");
     var popup = document.getElementById("popup");
     popup.classList.toggle("show");
+    val = val + correct;
+    document.getElementById("score").innerHTML = "Score: " + val;
+    
 }
 
 function red() {
@@ -12,4 +18,6 @@ function red() {
     document.getElementById("word2").classList.add("shake");
     var popuptushig = document.getElementById("popup2");
     popuptushig.classList.toggle("show2");
+    val = val - wrong;
+    document.getElementById("score").innerHTML = "Score: " + val;
 }
