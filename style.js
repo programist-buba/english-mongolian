6d884b14b932b6fd91017bd299bb417fb61a706d
Tushig Biliguun <Tushig@bachka.local>
2019-09-23 18:30:20 +0800

    adfds

--- style.js
+++ style.js
@@ -21,41 +21,33 @@ function red(v) {
     document.getElementById("score").innerHTML = "Score: " + val;
 }
 
-
-
 const db = firebase.firestore();
 const questions = db.collection('questions');
 
-// const q = questions.doc('oobcYsZKI1RAdqj7ISjx');
+//const q = questions.doc('0BTuyHcS4EJHKV04DGA4');
+function callquestion() {
+    var rnd = Math.floor(Math.random() * 5) + 1;
 
-var rnd = Math.floor(Math.random() * 4) + 1;
+    questions.where("level", "==", 1).where("question", "==", rnd)
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                console.log(doc.id, " => ", doc.data());
+                var item = doc.data();
+                document.getElementById("question").innerHTML = item.question;
 
-questions.where("level", "==", 1).where("random", "==", rnd)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            console.log(doc.id, " => ", doc.data());
-            var item = doc.data();
-            document.getElementById("question").innerHTML = item.question;
-
-            item.ans.forEach(function (i){
-                let el = createChoice(i.option, i.value, i.right);
-                document.getElementsByClassName('choice')[0].appendChild(el);
-            }) 
+                item.ans.forEach(function (i){
+                    let el = createChoice(i.option, i.value, i.right);
+                    document.getElementsByClassName('choice')[0].appendChild(el);
+                }) 
+            })
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
         });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
+}
+    callquestion();
 
-    //service cloud.firestore {
-        //match /databases/moen/documents; {
-            //match /VwKsk7GmVRmdMYlMbVvq/gamecreater/displayName/score/uid;
-            //match /VwKsk7GmVRmdMYlMbVvq/joiner/displayName/score/uid;
-           /// match /VwKsk7GmVRmdMYlMbVvq/questions/0;
-            //allow write;
-        //}
-    //}
 function createChoice(option, value, right) {
     // <div class="word" onclick="green()">
     //     <div id="letter">A</div>
@@ -66,8 +58,16 @@ function createChoice(option, value, right) {
     choice.addEventListener('click', function(){
         if (right == true){
             green(choice);
+            delete callquestion();
+            delete createChoice();
+            callquestion();
+            createChoice();
         } else {
             red(choice);
+            delete callquestion();
+            delete createChoice();
+            callquestion();
+            createChoice();
         }
     });
     let letter = document.createElement('div');
@@ -80,37 +80,37 @@ function createChoice(option, value, right) {
     return choice;
 }
 
-// q.get().then(function (doc) {
-//     if (doc.exists) {
-//         console.log("Document data:", doc.data());
-//         var item = doc.data();
-//         document.getElementById("question").innerHTML = item.question;
+// questions.get().then(function (doc) {
+//      if (doc.exists) {
+//          console.log("Document data:", doc.data());
+//          var item = doc.data();
+//          document.getElementById("question").innerHTML = item.question;
 
-//         item.ans.forEach(function (i){
-//             let el = createChoice(i.option, i.value, i.right);
-//             document.getElementsByClassName('choice')[0].appendChild(el);
-//         })
-//     } else {
-//         console.log("No such document!");
-//     }
-// }).catch(function (error) {
-//     console.log("Error getting document:", error);
+//          item.ans.forEach(function (i){
+//              let el = createChoice(i.option, i.value, i.right);
+//              document.getElementsByClassName('choice')[0].appendChild(el);
+//          })
+//      } else {
+//          console.log("No such document!");
+//      }
+//  }).catch(function (error) {
+//      console.log("Error getting document:", error);
 // });
 
-//questions.add({
-    //question: "Hello",
-    //level: 1,
-    //ans: [
-        //{option: 'A', value: 'TEST1', right: true},
-        //{option: 'B', value: 'TEST2', right: false},
-        //{option: 'C', value: 'TEST3', right: false},
-        //{option: 'D', value: 'TEST4', right: false},
-        //{option: 'E', value: 'TEST5', right: false}
-    //]
-//})
-//.then(function(docRef) {
-    //console.log("Document written with ID: ", docRef.id);
-//})
-//.catch(function(error) {
-    //console.error("Error adding document: ", error);
-//});
+// questions.add({
+//     question: "Level 2: 1",
+//     level: 2,
+//     ans: [
+//         {option: 'A', value: 'TEST1', right: true},
+//         {option: 'B', value: 'TEST2', right: false},
+//         {option: 'C', value: 'TEST3', right: false},
+//         {option: 'D', value: 'TEST4', right: false},
+//         {option: 'E', value: 'TEST5', right: false}
+//     ]
+// })
+// .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef.id);
+// })
+// .catch(function(error) {
+//     //console.error("Error adding document: ", error);
+// });
